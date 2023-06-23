b33d266ed77c600549eb80c1a125e44e0b29ad82
Aki <57061301+akiescarraga@users.noreply.github.com>
2023-06-23 09:56:19 +0800

    added all needed content for the footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./dist/**/*.{html,js}"],
   theme: {
+
     extend: {},
   },
   plugins: [],
